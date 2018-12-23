375470e8ae4501cd5481d15b53f257d5a2726683
sabudeg <bburakdegirmenci@gmail.com>
2018-12-23 17:52:49 +0300

    firstcommit

---
+++ CengAndroid/KisilerActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace CengAndroid
+{
+    [Activity(Label = "KisilerActivity")]
+    public class KisilerActivity : Activity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Kisiler);
+
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+            // Create your application here
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_about:
+                    StartActivity(typeof(MainActivity));
+                    return true;
+                case Resource.Id.navigation_kisiler:
+                    StartActivity(typeof(KisilerActivity));
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    StartActivity(typeof(MainActivity));
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CengAndroid/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace CengAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        TextView textMessage;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            textMessage = FindViewById<TextView>(Resource.Id.message);
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+        }
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_about:
+                    StartActivity(typeof(MainActivity));
+                    return true;
+                case Resource.Id.navigation_kisiler:
+                    StartActivity(typeof(KisilerActivity));
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    textMessage.SetText(Resource.String.title_notifications);
+                    StartActivity(typeof(MainActivity));
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
